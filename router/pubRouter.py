d238df1c68898633c81416acd779a27c2849c707
SeCorp-b <sem.koldewijn@student.hu.nl>
2024-04-08 23:43:26 +0200

    get_pub router fn name change

--- router/pubRouter.py
+++ router/pubRouter.py
@@ -11,7 +11,7 @@ router = APIRouter(prefix="/api/v1/pub", tags=["pub"])
 
 
 @router.get("/", response_model=List[GymaDTO], status_code=200)
-async def get_pub(gyma_keys: str = None, db: AsyncSession = Depends(get_db)):
+async def get_pub_three_latest(gyma_keys: str = None, db: AsyncSession = Depends(get_db)):
     logging.info(f"Searching for the latest three gyma entries {'excluding: ' + gyma_keys if gyma_keys else ''}")
 
     return await get_last_three_gyma_entry(gyma_keys, db)
