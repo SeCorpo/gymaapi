5f7fa33c793ba3d5ecce991e78e522ea4ddd5f1b
SeCorp-b <sem.koldewijn@student.hu.nl>
2024-04-20 02:06:34 +0200

    mineRouter and Service

--- dto/gymaDTO.py
+++ dto/gymaDTO.py
@@ -8,7 +8,7 @@ from dto.personDTO import PersonDTO
 
 
 class GymaDTO(BaseModel):
-    gyma_id: int = Field(...)
+    gyma_id: int = Field(...)  # gyma_id is used to exclude sending gyma from db when client has them in localstorage
     person: PersonDTO = Field(...)
     time_of_arrival: datetime = Field(...)
     time_of_leaving: datetime = Field(default=None)
---
+++ service/mineService.py
@@ -0,0 +1,31 @@
+from typing import List
+from fastapi import Depends
+from sqlalchemy import select, desc
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.ext.asyncio import AsyncSession
+from database import get_db
+
+from model.Gyma import Gyma
+
+
+async def get_last_three_gyma_entry_of_person(person_id: int, gyma_keys: str = None,
+                                              db: AsyncSession = Depends(get_db)) -> List[Gyma] | None:
+    """ Get last three gyma entries of person by time_of_leaving,
+    exclude gyma_keys, for the client already has them. """
+
+    try:
+        query = (
+            select(Gyma)
+            .order_by(desc(Gyma.time_of_leaving))
+            .limit(3)
+            .where(Gyma.person_id is person_id)
+            .filter(Gyma.gyma_id.notin_(gyma_keys.split(",") if gyma_keys else []))
+        )
+
+        result = await db.execute(query)
+        three_latest_gyma = result.scalars().all()
+
+        return list(three_latest_gyma)
+
+    except NoResultFound:
+        return None
